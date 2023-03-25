6ad68c55a827c2229c600c24c51ab8e52ea60776
Robert Siciliano <Robert.siciliano22@gmail.com>
2023-03-25 11:48:31 -0400

    Removed Unnecessary Files

--- server.js
+++ server.js
@@ -37,7 +37,6 @@ app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 // Prevent CORS Errors 
 app.use(function (req, res, next) {
